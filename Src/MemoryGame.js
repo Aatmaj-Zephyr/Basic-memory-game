a058a28b71e8024d1503da08e2b9fce2e8eae237
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-08-28 09:57:02 +0530

    Update MemoryGame.js

--- Src/MemoryGame.js
+++ Src/MemoryGame.js
@@ -1,98 +1,75 @@
-ArrayOfWords=[ "Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran♀","Nidorina","Nidoqueen","Nidoran♂","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Graveler","Golem","Ponyta","Rapidash","Slowpoke","Slowbro","Magnemite","Magneton","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix","Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr.","Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew","Chikorita","Bayleef","Meganium","Cyndaquil","Quilava","Typhlosion","Totodile","Croconaw","Feraligatr","Sentret","Furret","Hoothoot","Noctowl","Ledyba","Ledian","Spinarak","Ariados","Crobat","Chinchou","Lanturn","Pichu","Cleffa","Igglybuff","Togepi","Togetic","Natu","Xatu","Mareep","Flaaffy","Ampharos","Bellossom","Marill","Azumarill","Sudowoodo","Politoed","Hoppip","Skiploom","Jumpluff","Aipom","Sunkern","Sunflora","Yanma","Wooper","Quagsire","Espeon","Umbreon","Murkrow","Slowking","Misdreavus","Unown","Wobbuffet","Girafarig","Pineco","Forretress","Dunsparce","Gligar","Steelix","Snubbull","Granbull","Qwilfish","Scizor","Shuckle","Heracross","Sneasel","Teddiursa","Ursaring","Slugma","Magcargo","Swinub","Piloswine","Corsola","Remoraid","Octillery","Delibird","Mantine","Skarmory","Houndour","Houndoom","Kingdra","Phanpy","Donphan","Porygon2","Stantler","Smeargle","Tyrogue","Hitmontop","Smoochum","Elekid","Magby","Miltank","Blissey","Raikou","Entei","Suicune","Larvitar","Pupitar","Tyranitar","Lugia","Ho-Oh","Celebi","Treecko","Grovyle","Sceptile","Torchic","Combusken","Blaziken","Mudkip","Marshtomp","Swampert","Poochyena","Mightyena","Zigzagoon","Linoone","Wurmple","Silcoon","Beautifly","Cascoon","Dustox","Lotad","Lombre","Ludicolo","Seedot","Nuzleaf","Shiftry","Taillow","Swellow","Wingull","Pelipper","Ralts","Kirlia","Gardevoir","Surskit","Masquerain","Shroomish","Breloom","Slakoth","Vigoroth","Slaking","Nincada","Ninjask","Shedinja","Whismur","Loudred","Exploud","Makuhita","Hariyama","Azurill","Nosepass","Skitty","Delcatty","Sableye","Mawile","Aron","Lairon","Aggron","Meditite","Medicham","Electrike","Manectric","Plusle","Minun","Volbeat","Illumise","Roselia","Gulpin","Swalot","Carvanha","Sharpedo","Wailmer","Wailord","Numel","Camerupt","Torkoal","Spoink","Grumpig","Spinda","Trapinch","Vibrava","Flygon","Cacnea","Cacturne","Swablu","Altaria","Zangoose","Seviper","Lunatone","Solrock","Barboach","Whiscash","Corphish","Crawdaunt","Baltoy","Claydol","Lileep","Cradily","Anorith","Armaldo","Feebas","Milotic","Castform","Kecleon","Shuppet","Banette","Duskull","Dusclops","Tropius","Chimecho","Absol","Wynaut","Snorunt","Glalie","Spheal","Sealeo","Walrein","Clamperl","Huntail","Gorebyss","Relicanth","Luvdisc","Bagon","Shelgon","Salamence","Beldum","Metang","Metagross","Regirock","Regice","Registeel","Latias","Latios","Kyogre","Groudon","Rayquaza","Jirachi","Deoxys","Turtwig","Grotle","Torterra","Chimchar","Monferno","Infernape","Piplup","Prinplup","Empoleon","Starly","Staravia","Staraptor","Bidoof","Bibarel","Kricketot","Kricketune","Shinx","Luxio","Luxray","Budew","Roserade","Cranidos","Rampardos","Shieldon","Bastiodon","Burmy","Wormadam","Mothim","Combee","Vespiquen","Pachirisu","Buizel","Floatzel","Cherubi","Cherrim","Shellos","Gastrodon","Ambipom","Drifloon","Drifblim","Buneary","Lopunny","Mismagius","Honchkrow","Glameow","Purugly","Chingling","Stunky","Skuntank","Bronzor","Bronzong","Bonsly","Mime","Jr.","Happiny","Chatot","Spiritomb","Gible","Gabite","Garchomp","Munchlax","Riolu","Lucario","Hippopotas","Hippowdon","Skorupi","Drapion","Croagunk","Toxicroak","Carnivine","Finneon","Lumineon","Mantyke","Snover","Abomasnow","Weavile","Magnezone","Lickilicky","Rhyperior","Tangrowth","Electivire","Magmortar","Togekiss","Yanmega","Leafeon","Glaceon","Gliscor","Mamoswine","Porygon-Z","Gallade","Probopass","Dusknoir","Froslass","Rotom","Uxie","Mesprit","Azelf","Dialga","Palkia","Heatran","Regigigas","Giratina","Cresselia","Phione","Manaphy","Darkrai","Shaymin","Arceus","Victini","Snivy","Servine","Serperior","Tepig","Pignite","Emboar","Oshawott","Dewott","Samurott","Patrat","Watchog","Lillipup","Herdier","Stoutland","Purrloin","Liepard","Pansage","Simisage","Pansear","Simisear","Panpour","Simipour","Munna","Musharna","Pidove","Tranquill","Unfezant","Blitzle","Zebstrika","Roggenrola","Boldore","Gigalith","Woobat","Swoobat","Drilbur","Excadrill","Audino","Timburr","Gurdurr","Conkeldurr","Tympole","Palpitoad","Seismitoad","Throh","Sawk","Sewaddle","Swadloon","Leavanny","Venipede","Whirlipede","Scolipede","Cottonee","Whimsicott","Petilil","Lilligant","Basculin","Sandile","Krokorok","Krookodile","Darumaka","Darmanitan","Maractus","Dwebble","Crustle","Scraggy","Scrafty","Sigilyph","Yamask","Cofagrigus","Tirtouga","Carracosta","Archen","Archeops","Trubbish","Garbodor","Zorua","Zoroark","Minccino","Cinccino","Gothita","Gothorita","Gothitelle","Solosis","Duosion","Reuniclus","Ducklett","Swanna","Vanillite","Vanillish","Vanilluxe","Deerling","Sawsbuck","Emolga","Karrablast","Escavalier","Foongus","Amoonguss","Frillish","Jellicent","Alomomola","Joltik","Galvantula","Ferroseed","Ferrothorn","Klink","Klang","Klinklang","Tynamo","Eelektrik","Eelektross","Elgyem","Beheeyem","Litwick","Lampent","Chandelure","Axew","Fraxure","Haxorus","Cubchoo","Beartic","Cryogonal","Shelmet","Accelgor","Stunfisk","Mienfoo","Mienshao","Druddigon","Golett","Golurk","Pawniard","Bisharp","Bouffalant","Rufflet","Braviary","Vullaby","Mandibuzz","Heatmor","Durant","Deino","Zweilous","Hydreigon","Larvesta","Volcarona","Cobalion","Terrakion","Virizion","Tornadus","Thundurus","Reshiram","Zekrom","Landorus","Kyurem","Keldeo","Meloetta","Genesect","Chespin","Quilladin","Chesnaught","Fennekin","Braixen","Delphox","Froakie","Frogadier","Greninja","Bunnelby","Diggersby","Fletchling","Fletchinder","Talonflame","Scatterbug","Spewpa","Vivillon","Litleo","Pyroar","Flabébé","Floette","Florges","Skiddo","Gogoat","Pancham","Pangoro","Furfrou","Espurr","Meowstic","Honedge","Doublade","Aegislash","Spritzee","Aromatisse","Swirlix","Slurpuff","Inkay","Malamar","Binacle","Barbaracle","Skrelp","Dragalge","Clauncher","Clawitzer","Helioptile","Heliolisk","Tyrunt","Tyrantrum","Amaura","Aurorus","Sylveon","Hawlucha","Dedenne","Carbink","Goomy","Sliggoo","Goodra","Klefki","Phantump","Trevenant","Pumpkaboo","Gourgeist","Bergmite","Avalugg","Noibat","Noivern","Xerneas","Yveltal","Zygarde","Diancie"]
-//Array containing the words.
+var lastbutton = null; //Last button that was clicked. 
+var score = 0; //Score, i.e. how many cards are opened
+var turns = 0; //Number of turns taken.
 
-const Word=ArrayOfWords[Math.floor(Math.random()*(ArrayOfWords.length))];
-//One random word from the Array
+const Cards = []; //Array to store the Cards
+//Setting the cards
+for (let j = 0; j < 8; j++) {
+    r = Math.floor(Math.random() * (90 - 64 + 1)) + 64; //Character set from unicode
+    Cards.push(String.fromCharCode(r)); //Push twice.
+    Cards.push(String.fromCharCode(r));
+}
+Cards.sort(() => .5 - Math.random()); //Random shuffling of cards.
+Cards.push(Cards[0]); //Ignore the card at 0th positon
 
-const WordLength=Word.length;
-//Length of the word
-
-const WordLetters=[];
-//Array to store the letters of the Word
-WordLetters.push(Word.charAt(0)); //Push the First letter.
-//In the current version, only the first letter is visible at a hint.
-//Modify it further to include random letters, but be careful of the capital letter.
-
-for(let i=WordLength-1;i>0;i--){
-    WordLetters.push("_"); 
-    //Push blank underscores in the rest of the array according t the length of the word.
+function Buttonclicked(a) {
+    //Function to be executed once button is clicked. 
+    //a is the parameter which tells which button is clicked.
+    document.getElementById("button" + a).innerHTML = Cards[a]; //Flip the card over
+    document.getElementById("button" + a).disabled = "disabled"; //Disable clicking the same card
+    checkscore(a); //Check the cards.
 }
 
-document.getElementById("Word").innerHTML=WordLetters.join(" ");
-//Display the word in the html file
+function checkscore(a) {
+    if (lastbutton == null) {
+        lastbutton = a;
+        //If it is the first Card (from two cards) to be chosen,
+        //then set the value of lastbutton to the button pressed.
+    } else {
+        if (Cards[a] != Cards[lastbutton]) { //IF they don't match.....
+            temp = lastbutton;
+            document.getElementById("button" + a).style = "background-color:Orange";
+            document.getElementById("button" + temp).style = "background-color:Orange";
+            //If invalid, then set the background background-color Orange.
+            window.setTimeout(function() {
+                document.getElementById("button" + a).innerHTML = "?";
+                document.getElementById("button" + a).style = "background-color:";
+                document.getElementById("button" + temp).style = "background-color:";
+                document.getElementById("button" + temp).innerHTML = "?";
+                document.getElementById("button" + temp).disabled = "";
+                document.getElementById("button" + a).disabled = "";
+            }, 1000);
+            //With delay, flip them back. and enable clicking.
 
-let TurnsLeft=Math.floor(WordLength*0.8);
-//Number of turns left. Modify the formulae in the later versions.
-
-document.getElementById("TurnsLeft").innerHTML="Turns left= "+TurnsLeft;
-//Display the number of turns left
-
-const Letters=[];//Array to store the Letters
-//Setting the Letters
-
-for (var k of Word){ 
-    if(!(Letters.includes(k))&k!=Word.charAt(0)){
-       Letters.push(k.toString());  
-       //Push the letters of word into the "Letters" array without repeatition
-    }
-}
-while(Letters.length<16){
-    r=Math.floor(Math.random()*(122-97+1))+97; //Character set from unicode
-    v=String.fromCharCode(r);
-    if(!(Letters.includes(v))){
-        Letters.push(v);
-    }
-}
-Letters.sort( () => 0.5 - Math.random() ); //Random shuffling of Letters.
-Letters.push(Letters[0]);//Ignore the card at 0th positon
-for(let f=1;f<=16;f++){
-    document.getElementById("button"+f).innerHTML=Letters[f]; 
-}
-function ButtonClicked(a){ 
-//Function to be executed once button is clicked. 
-//a is the parameter which tells which button is clicked.
-    document.getElementById("button"+a).disabled = "disabled"; //Disable clicking the same card
-    checkscore(a); //Check the Letters.
-    }
-  
-function checkscore(a){
-    if(Word.includes(Letters[a])){
-        for(let i=Word.length;i>=0;i--){
-    if(Word.charAt(i)==Letters[a]){
-        WordLetters[i]=Letters[a]
-    }
-            }
-        //WordLetters.push(Letters[a]);
-        document.getElementById("button"+a).style="background-color:lime";
-        document.getElementById("Word").innerHTML=WordLetters.join(" ");
+            lastbutton = null; //reset lastbutton
+        } else {
+            //If they match.... 
+            score = score + 1; //Increase score
+            document.getElementById("button" + a).style = "background-color:lime;"
+            document.getElementById("button" + lastbutton).style = "background-color:lime;"
+            //Change their colours to green
+            document.getElementById("button" + lastbutton).disabled = "disabled";
+            document.getElementById("button" + a).disabled = "disabled";
+            //Disable them from further clicking.
+            document.getElementById("score").innerHTML = "Score=" + score;
+            //display score
+            lastbutton = null
         }
-        else{
-             document.getElementById("button"+a).style="background-color:Orange";
-             TurnsLeft=TurnsLeft-1;
+
+        turns = turns + 1; //Increase number of turns
+        document.getElementById("Turns").innerHTML = "Turns taken=" + turns;
+        //display the number of turns taken.
+        if (score == (Cards.length - 1) / 2) {
+            gameover(); //game over.
         }
-      document.getElementById("TurnsLeft").innerHTML="Turns left= "+TurnsLeft;
-      if(WordLetters.join("")==Word){
-          GameWon();
-      }
-       if(TurnsLeft==0){
-            GameOver(); //game over.
-       }
-    
+    }
 }
-function GameOver(){
-        window.setTimeout(function (){
-         alert("Game Over, you lost");
+
+function gameover() {
+    window.setTimeout(function() {
+        alert("Game Over");
         //alert that the game is over after a delay
-        },1000)
-       document.getElementById("Word").innerHTML=Word;
-       document.getElementById("Word").style="color:Red";
-}
-function GameWon(){
-    document.getElementById("Word").innerHTML=Word;
-       document.getElementById("Word").style="color:lime";
-   window.setTimeout(function (){
-       
-         alert("Game Over, you Won!");
-        //alert that the game is over after a delay
-        },1000)
+    }, 1000)
+    document.getElementById("score").innerHTML = "Game over!";
+    document.getElementById("Turns").innerHTML = "You took " + turns + " turns";
 }
